09d318061afe2f6e1ba884a304220617b04c9b50
vlemert <niels.vleeming@gmail.com>
2014-10-24 21:21:54 +0200

    fixed

--- fall-ready-worker.js
+++ fall-ready-worker.js
@@ -1,3 +1,26 @@
-var FallReadyWorker = function(){
+var FallReadyWorker = function(options){
 
+    this.say = function(e) {
+        if (this.worker) {
+            this.worker.postMessage(e);
+        } else {
+            try {
+                var result = options.worker.func(e);
+                options.onMessage(result);
+            } catch(e) {
+                options.onError(e);
+            }
+        }
+    };
+
+    if (Worker) {
+        var myWorker = "var func = " + options.worker.func.toString() + "; var getPostData = " + options.worker.getPostData.toString() + "; self.addEventListener('message', function(e) { self.postMessage(getPostData(e))}); self.addEventListener('error', " + options.onError + ");";
+
+        var blob = new Blob([String(myWorker)]),
+            blobUrl = window.URL.createObjectURL(blob);
+
+        this.worker = new Worker(blobUrl);
+
+        this.worker.addEventListener('message', options.onMessage);
+    }
 };
